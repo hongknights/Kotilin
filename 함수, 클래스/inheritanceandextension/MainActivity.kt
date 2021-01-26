ecc367fb5bcfa27d37dc604e798750a2cf66b3ff
Hong Seong Hoon <61689222+hongknights@users.noreply.github.com>
2021-01-26 18:46:06 +0900

    Add files via upload

---
+++ 함수, 클래스/inheritanceandextension/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.inheritanceandextension
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        testStringExtension()
+    }
+
+    // String 익스텐션 테스트 하기
+    fun testStringExtension() {
+        var original = "Hello"
+        var added = " Guys~"
+
+        //plus 함수를 사용해서 문자열을 더할 수 있습니다.
+        Log.d("Extension", "added를 더한 값은 ${original.plus(added)}입니다.")
+    }
+
+    fun String.plus(word : String): String {
+        return this + word
+    }
+}
